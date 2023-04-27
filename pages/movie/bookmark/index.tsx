b30409fbecc7f84096a0bb7a29d286c92961348b
Inaboya <momohdavid21@gmail.com>
2023-04-27 01:26:38 +0100

    remove redundant code

--- pages/movie/bookmark/index.tsx
+++ pages/movie/bookmark/index.tsx
@@ -13,7 +13,7 @@ const BookmarkMovies = () => {
   return (
     <Box bg="gray.700" pb={10} px={4} maxW="7xl" mx="auto">
       <Heading color="white" fontSize="2xl" mt={8} mb={5}>
-        What's Popular
+        What's Popular na
       </Heading>
       <Grid
         gap={4}
@@ -23,8 +23,6 @@ const BookmarkMovies = () => {
           lg: 'repeat(5, 1fr)',
         }}
       >
-        {/* {data ?  movies.map((movie:any) => (<MovieCard movie={movie} key={movie.id} />): (<Heading color="white" fontSize="2xl" mt={8} mb={5}>No Movies</Heading>)} */}
-
         {data ? (
           movies.map((movie: any) => <MovieCard movie={movie} key={movie.id} />)
         ) : (
